7aac13eafb29dc4f4aa2823abd88c048c9e58af3
fran <francesco.theuma@gmail.com>
2024-10-07 20:07:23 +0200

    lol

---
+++ build/CMakeFiles/Working.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Working.dir/main.cpp.o"
+  "CMakeFiles/Working.dir/main.cpp.o.d"
+  "Working"
+  "Working.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Working.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
